2201f662a165c1f6ec9f38fa2fca66268f249b63
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-08-12 21:10:08 -0600

    Create of clustering file
    Hierarchical Clustering in R

---
+++ clustering.R
@@ -0,0 +1,5 @@
+set.seed(1234)
+x <- rnorm(12, rep(1:3, each = 4), 0.2)
+y <- rnorm(12, rep(c(1, 2, 1), each = 4), 0.2)
+plot(x, y, col = "blue", pch = 19, cex = 2)
+text(x + 0.05, y + 0.05, labels = as.character(1:12))
